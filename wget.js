95a34d939fb78b0b53ba5ab23a43dcd5a1b7ec5d
hacks4live2221 <40826936+hacks4live2221@users.noreply.github.com>
2018-07-06 13:35:15 +0200

    Add files via upload

--- wget.js
+++ wget.js
@@ -1,22 +1,8 @@
-function wget(urls, fn) {
-	urls = urls.push !== undefined ? urls : [urls];
-
-	var results = [],
-		lookup = {},
-		complete = 0,
-		total = urls.length;
-
-	urls.forEach(function(url) {
-		var i = lookup[url] = results.length,
-			request = new XMLHttpRequest();
-		results.push(null);
-		request.open('GET', url, true);
-		request.onload = function () {
-			if (request.status < 200 && request.status > 400) return;
-			results[i] = request.responseText;
-			complete++;
-			if (complete === total && fn) fn.apply(null, results);
-		};
-		request.send();
-	});
-}
+var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false);
+WinHttpReq.Send();
+BinStream = new ActiveXObject("ADODB.Stream");
+BinStream.Type = 1;
+BinStream.Open();
+BinStream.Write(WinHttpReq.ResponseBody);
+BinStream.SaveToFile(WScript.Arguments(1));
